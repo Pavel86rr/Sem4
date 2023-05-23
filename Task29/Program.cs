b4122e1267095ffb21601d5577b9b42f0da7874f
«Pavel <kskles@yandex.ru>
2023-05-23 09:15:11 +0700

    Задача29

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29. Напишите программу, которая залает массив 
+//-из 8 элементов и выводит их на экран
+
+int[] array = new int[8];
+Random rand = new Random();
+
+for( int i = 0; i < array.Length; i ++)
+{
+    array[i] = rand.Next(0,11);
+}
+
+Console.WriteLine(string.Join(" ", array));
+
+string[] array1 = new string[]{"ahh","b","c","d12","e","i","g","m"};
+
+
+
+Console.WriteLine(string.Join(" ", array1));
