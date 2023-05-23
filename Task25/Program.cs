be6250b160488823a56e3c3c290f7e59210f910d
«Pavel <kskles@yandex.ru>
2023-05-23 22:23:13 +0700

    Задача25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два 
+// числа (А и В) и возводит А в натуральную степень В
+
+Console.WriteLine("Введите два целых числа: ");
+int [] number = Array.ConvertAll(Console.ReadLine()!.Split(),int.Parse);
+//System.Console.WriteLine("Вот Ваши числа: ");
+//System.Console.WriteLine(string.Join(" ", number));
+
+int A = number [0];
+int b = number [1];
+
+int pow(int A, int b)
+{
+int t = A;
+for (int i = 1; i < b; i++)
+    {
+      A=A*t;
+    }
+   return A;
+}
+Console.WriteLine(pow(A,b));    
